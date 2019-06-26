e2aacca8d238edff08bb1868e7b8c30e83f74f72
grv1198 <grv1198@gmail.com>
2019-06-27 00:27:04 +0530

    Linked list node

---
+++ src/com/grv/linkedlist/LLNode.java
@@ -0,0 +1,29 @@
+package com.grv.linkedlist;
+
+public class LLNode {
+
+	
+	int data;
+	
+	LLNode next;
+	public LLNode(int data) {
+		// TODO Auto-generated constructor stub
+		this.data=data;
+		
+		
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public LLNode getNext() {
+		return next;
+	}
+	public void setNext(LLNode next) {
+		this.next = next;
+	} 
+	
+	
+}
