bd06ab7dd1f64fcd353e2c47ae5204137de773e9
grv1198 <grv1198@gmail.com>
2019-07-24 10:53:00 +0530

    DLL FOR LRU

---
+++ src/com/grv/systemdesignlowlevel/lrucache/DoublyLL.java
@@ -0,0 +1,40 @@
+package com.grv.systemdesignlowlevel.lrucache;
+
+public class DoublyLL {
+
+        private int data;
+
+        private DoublyLL next;
+
+        private DoublyLL prev;
+
+
+        public DoublyLL(int data) {
+            this.data = data;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public DoublyLL getNext() {
+            return next;
+        }
+
+        public void setNext(DoublyLL next) {
+            this.next = next;
+        }
+
+        public DoublyLL getPrev() {
+            return prev;
+        }
+
+        public void setPrev(DoublyLL prev) {
+            this.prev = prev;
+        }
+    }
+
