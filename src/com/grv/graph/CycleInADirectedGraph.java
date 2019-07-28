5efb5a64d7926ed80e1c966d5662b4a2f0d95a1b
grv1198 <grv1198@gmail.com>
2019-07-28 17:02:07 +0530

    detect in cycle in directed graph

---
+++ src/com/grv/graph/CycleInADirectedGraph.java
@@ -0,0 +1,53 @@
+package com.grv.graph;
+
+import java.util.Iterator;
+
+
+public class CycleInADirectedGraph {
+
+
+
+    public  static boolean isCyclicUtil(Integer v, boolean[] visited, boolean[] recStackVisited, Graph g) {
+
+        if (recStackVisited[v]) {
+            //  same vertex visited twice in current dfs, cycle exists
+            return true;
+        }
+
+        if (!visited[v]) {// if dfs has not been done for this vertex
+
+            recStackVisited[v] = true;
+            visited[v] = true;
+
+            Iterator<Integer> it= g.adj[v].iterator();
+
+            while(it.hasNext()){
+
+                Integer c = it.next();
+                if (isCyclicUtil(c, visited, recStackVisited, g)) {
+                    return true;
+                }
+            }
+
+        }
+
+        recStackVisited[v] = false;
+        return false;
+
+    }
+
+
+
+    public  static boolean isCyclic(Graph g){
+
+        boolean visited[] = new boolean[g.getV()];
+        boolean recStackVisited[] = new boolean[g.getV()];
+
+        for (int i = 0; i < g.getV(); i++)
+            if (isCyclicUtil(i, visited, recStackVisited, g))
+                return true;
+
+        return false;
+
+    }
+}
